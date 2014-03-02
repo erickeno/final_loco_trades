9d7b9137e9ad1b56e0ee6fe1fc55723f76f49cf0
Erick B <erickeno@yahoo.com>
2014-03-02 17:18:27 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LocoTrades::Application.config.secret_key_base = 'fedfb21c9b0ea40e286cf1192933064dc93f79c2ec07395cda516d3959702e5b406485ef38c65c4117a8eddcb7933f23a7479eea16c741bed79f50c459c44f00'
